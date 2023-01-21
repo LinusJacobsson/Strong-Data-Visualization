12f301c22afa0f0200e8d01a55c6e7db50afef2c
LinusJacobsson <jacobssonlinus00@gmail.com>
2023-01-21 15:38:38 +0100

    added docstrings

--- strong.py
+++ strong.py
@@ -20,24 +20,33 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("strong.csv")
 
 cleaned = df[["Date", "Workout Name", "Duration", "Exercise Name", "Set Order", "Weight", "Reps"]]
-print(cleaned)
+#print(cleaned)
 
 def get_date_range(data_frame, start_date, end_date):
+    """ Slices a dataframe between a start date and end date"""
     return data_frame[(data_frame["Date"] > start_date) & (data_frame["Date"] < end_date)]
 
 
 def workouts_between_dates(data_frame, start_date, end_date):
+    """ Takes a dataframe and two dates and prints the number
+        of workouts between the dates.
+    """
     date_range = get_date_range(data_frame, start_date, end_date)
     unique_dates = date_range['Date'].nunique()
     print(f'During this period, you have completed {unique_dates} workouts!')
 
 
 def number_of_exercises(data_frame, start_date, end_date):
+    """ Takes a dataframe and two dates and prints the number
+        of unique exercises performed during that period.
+    """
     date_range = get_date_range(data_frame, start_date, end_date)
     unique_exercises = date_range['Exercise Name'].nunique()
     print(f'During this period, you have performed {unique_exercises} different exercises!')
 
 
 
+def one_rep_max(data_frame, exercise):
+    pass
 workouts_between_dates(cleaned, '2022-08-01', '2023-01-15')
 number_of_exercises(cleaned, '2022-078-01', '2023-01-15')
