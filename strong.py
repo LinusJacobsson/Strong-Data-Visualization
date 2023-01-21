bfbdc1b6a57a6a8d9d15f4d2b11d1cc8da76ed5f
LinusJacobsson <jacobssonlinus00@gmail.com>
2023-01-21 18:45:11 +0100

    added linear regression to plot function

--- strong.py
+++ strong.py
@@ -15,6 +15,8 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as plt_date
 
 
 df = pd.read_csv("strong.csv")
@@ -49,9 +51,9 @@ def number_of_exercises(data_frame, start_date, end_date):
 
 def one_rep_max_row(row):
     """ Takes a row in a dataframe, and calculates the
-    theoretical 1 rep max using the Brzycki formula:
-     weight * (36 / (37 - reps)), using the 'Weight' 
-     and 'Reps' column.
+        theoretical 1 rep max using the Brzycki formula:
+        weight * (36 / (37 - reps)), using the 'Weight' 
+        and 'Reps' column.
     """
     weight = row['Weight']
     reps = row['Reps']
@@ -61,7 +63,7 @@ def one_rep_max_row(row):
 
 def one_rep_max_exercise(dataframe, exercise):
     """ Applies the one_rep_max_row function to every row in a dataframe
-    with the exercise name 'exercise', and then returns a list of the result.
+        with the exercise name 'exercise', and then returns a list of the result.
     """ 
     squat_rows = dataframe.loc[df['Exercise Name'] == exercise]
     max =  squat_rows[['Weight', 'Reps', 'Date']].apply(one_rep_max_row, axis=1)
@@ -71,10 +73,14 @@ def one_rep_max_exercise(dataframe, exercise):
 def plot_max(date_weight_tuples, exercise):
     """Creates a scatter plot of the calculated one rep max
         vs the dates.
-         """
-    dates = [date[1] for date in date_weight_tuples]
-    maxes = [max[0] for max in date_weight_tuples]
+    """
+    dates = plt_date.date2num(np.array([date[1] for date in date_weight_tuples]))
+    maxes = np.array([max[0] for max in date_weight_tuples])
+    coefficients = np.polyfit(dates, maxes, 1)
+    polynomial = np.poly1d(coefficients)
+    ys = polynomial(dates)
     plt.plot_date(dates, maxes)
+    plt.plot(dates, ys)
     plt.title(f'Calculated 1-RM for {exercise}')
     plt.xlabel('Date')
     plt.ylabel('Weight [kg]')
